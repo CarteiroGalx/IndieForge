bc79326db520b09cb93104eddd03a4b4476068d7
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-06-24 09:35:58 -0300

    fix: recria migration inicial

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IndieForge.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace IndieForge.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Projeto> Projects { get; set; }
+        public DbSet<Contribuicao> Contribuicoes { get; set; }
     }
 }
