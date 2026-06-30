bf0b1cc6c61e99f0b5becc54d8b830ab21a9b768
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-06-30 10:59:29 -0300

    feat: add reforma classe Projeto

---
+++ Migrations/20260630133922_ReformaClasseProjeto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IndieForge.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReformaClasseProjeto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalArrecadado",
+                table: "Projects",
+                newName: "DataCriacao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataCriacao",
+                table: "Projects",
+                newName: "TotalArrecadado");
+        }
+    }
+}
