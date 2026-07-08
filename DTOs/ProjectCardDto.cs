aea4043b1f6d0311000ea5e422ab129eb530b818
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-07-08 18:08:50 -0300

    feat: remove construtor de dto de card de projeto

--- DTOs/ProjectCardDto.cs
+++ DTOs/ProjectCardDto.cs
@@ -6,14 +6,14 @@ using IndieForge.Models;
 
 namespace IndieForge.DTOs
 {
-    public class ProjectCardDto(string nome, string descricao, decimal meta, decimal arrecadado, Status status, DateTime dataCriacao, string criadorNome)
+    public class ProjectCardDto()
     {
-        public string Nome { get; set; } = nome;
-        public string Descricao { get; set; } = descricao;
-        public decimal Meta { get; set; } = meta;
-        public decimal Arrecadado { get; set; } = arrecadado;
-        public DateTime DataCriacao { get; set; } = dataCriacao;
-        public Status Status { get; set; } = status;
-        public string CriadorNome { get; set; } = criadorNome;
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Meta { get; set; }
+        public decimal Arrecadado { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public Status Status { get; set; }
+        public string CriadorNome { get; set; }
     }
 }
