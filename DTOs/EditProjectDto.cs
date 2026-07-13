9d5e1996304006a1b70d4f19380641d89f26b514
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-07-13 19:37:55 -0300

    fix: cria dto de edicao de projeto

---
+++ DTOs/EditProjectDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IndieForge.DTOs
+{
+    public class EditProjectDto
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 200 caracteres")]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A descrição do projeto é obrigatória")]
+        [StringLength(2000, MinimumLength = 10, ErrorMessage = "A descrição deve ter entre 10 e 2000 caracteres")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
