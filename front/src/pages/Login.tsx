81c21d23b422d51416e61806fbac1be7655755c4
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-07-27 11:50:11 -0300

    feat(front): cria tela de carregamento

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Login() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const handleLogin = async (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -19,6 +21,11 @@ export default function Login() {
   return (
     <div className="d-flex align-items-center justify-content-center vh-100" style={{ backgroundColor: '#0a0a0a' }}>
       <div className="card shadow-lg my-3" style={{ width: '380px', border: '0', borderRadius: '1rem', backgroundColor: '#111111' }}>
+        {loading && (
+          <div className="rounded h-100 w-100 text-white d-flex" style={{ backgroundColor: '#111111', position: 'absolute', top: 0, left: 0, zIndex: 10, justifyContent: 'center', alignItems: 'center' }}>
+            <h2>Carregando...</h2>
+          </div>
+        )}
         <div className="card-body p-3">
           <div className="col-12 text-center mb-4">
             <img className='col-4' src={logo} alt="IndieForge Logo" />
@@ -27,7 +34,6 @@ export default function Login() {
             </h2>
             <p className="text-white-50">Faça login para acessar sua área de creator.</p>
           </div>
-
           <form onSubmit={handleLogin}>
             <div className="mb-3">
               <label htmlFor="username" className="form-label text-white">
