b78e8b90be5bd4cf71134c38b8564751a1f0ba50
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2026-08-05 18:23:15 -0300

    refactor(front): mudança de tipagem

--- front/src/pages/Register/Register.tsx
+++ front/src/pages/Register/Register.tsx
@@ -11,7 +11,7 @@ export default function Register() {
     const [error, setError] = useState<string | null>(null)
     const navigate = useNavigate()
 
-    const handleRegister = async (event: React.FormEvent<HTMLFormElement>) => {
+    const handleRegister = async (event: React.SubmitEvent<HTMLFormElement>) => {
         event.preventDefault()
         setLoading(true)
         setError(null)
